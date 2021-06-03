63878673cb57f61a123c14fb4645a0d7b5f4ca66
Fachizalzul <fachrizalzul17@gmail.com>
2021-06-03 23:22:17 +0700

    perkembangan 1

---
+++ bahasa6.py
@@ -0,0 +1,16 @@
+import bahasa6_lexer
+import bahasa6_parser
+import bahasa6_interp
+
+from sys import *
+
+#DENGAN MASUKAN EKSTENSI .COD
+lexer = bahasa6_lexer.BasicLexer()
+parser = bahasa6_parser.BasicParser()
+env = {}
+
+file = open(argv[1])
+text = file.readlines()
+for line in text:
+    tree = parser.parse(lexer.tokenize(line))
+    bahasa6_interp.BasicExecute(tree, env)
